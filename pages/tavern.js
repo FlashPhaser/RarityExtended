f352bc33b6ef245cbde4116a9f649ee0636746a0
Kiven <abysus@163.com>
2021-09-09 12:02:38 +0800

    new

--- pages/tavern.js
+++ pages/tavern.js
@@ -12,67 +12,67 @@ import	useWeb3						from	'contexts/useWeb3';
 
 const	classes = {
 	'Barbarian': {
-		name: 'Barbarian',
+		name: '狂战士(Barbarian)',
 		img: '/barbarian.png',
 		description: 'No friend of the books, unlike any librarian.\nStrength, weapons, and anger serve the Barbarian',
 		id: 1,
 	},
 	'Bard': {
-		name: 'Bard',
+		name: '吟游诗人(Bard)',
 		img: '/bard.png',
 		description: 'Words, songs, and music are certainly not hard.\n The magic of the voice is the weapon of the Bard',
 		id: 2,
 	},
 	'Cleric': {
-		name: 'Cleric',
+		name: '牧师(Cleric)',
 		img: '/cleric.png',
 		description: 'In the world of adventure, pains and wounds are quite generic.\nIf you live a life or danger, you best know a Cleric',
 		id: 3,
 	},
 	'Druid': {
-		name: 'Druid',
+		name: '德鲁伊(Druid)',
 		img: '/druid.png',
 		description: 'All life is connected in something that is rather fluid.\nThe trees, insects, and animals are all friends of the Druid',
 		id: 4,
 	},
 	'Fighter': {
-		name: 'Fighter',
+		name: '战士(Fighter)',
 		img: '/fighter.png',
 		description: 'Scorn should not be directed at one with a dream to be a writer.\nBut tactics and sword play are what drive the Fighter',
 		id: 5,
 	},
 	'Monk': {
-		name: 'Monk',
+		name: '僧侣(Monk)',
 		img: '/monk.png',
 		description: 'Some pursue vanity, and others just want to get drunk.\nInner peace, and control of the body are the goals of a monk',
 		id: 6,
 	},
 	'Paladin': {
-		name: 'Paladin',
+		name: '圣骑士(Paladin)',
 		img: '/paladin.png',
 		description: 'Some hearts when inspected are found with malice therein.\nBut righteous and honor are the tenets of the Paladin',
 		id: 7,
 	},
 	'Ranger': {
-		name: 'Ranger',
+		name: '游侠(Ranger)',
 		img: '/ranger.png',
 		description: 'Most, avoid, flee, and fear only a little bit of danger.\nWith a bow in the wilderness, you might find a Ranger',
 		id: 8,
 	},
 	'Rogue': {
-		name: 'Rogue',
+		name: '盗贼(Rogue)',
 		img: '/rogue.png',
 		description: 'The rich are rich and the poor are poor is in vogue.\nBut with sticky fingers and sharp daggers you find the Rogue',
 		id: 9,
 	},
 	'Sorcerer': {
-		name: 'Sorcerer',
+		name: '魔法师(Sorcerer)',
 		img: '/sorcerer.png',
 		description: 'A scholarly teacher of magic seems like a torturer.\nBut this is not of concern to the innate magic of a Sorcerer',
 		id: 10,
 	},
 	'Wizard': {
-		name: 'Wizard',
+		name: '术士(Wizard)',
 		img: '/wizard.png',
 		description: 'Many waste their time on a log, idle like a lizard.\nHowever, through study, immense power is granted to the Wizard',
 		id: 11,
@@ -118,42 +118,34 @@ function	Class({provider, rarityClass, fetchRarity, router}) {
 	);
 }
 
-function	Index({fetchRarity, router}) {
-	const	{provider} = useWeb3();
-	const	[option, set_option] = useState(0);
+return (
+	<section className={'mt-12'}>
 
-	return (
-		<section className={'mt-12'}>
-			<div className={'max-w-screen-lg w-full mx-auto'}>
-				<div className={'flex flex-row items-center'}>
-					<div className={'w-60 mr-16'}>
-						<Image
-							src={'/avatar/banker.png'}
-							loading={'eager'}
-							quality={100}
-							width={240}
-							height={240} />
-					</div>
-					<h1 className={'text-lg whitespace-pre-line justify-center mb-4'}>{'Hello traveler! Welcome to Facu\'s Tavern.\nWhat do you want to do ?'}</h1>
+		<div className={'max-w-screen-lg w-full mx-auto'}>
+			<h1 className={'font-title text-lg uppercase justify-center mb-4'}>{'远方的旅人，欢迎你来到冒险者招募营地！'}</h1>
+			<p className={'font-title text-base uppercase'}>{'你想要做点什么呢 ?'}</p>
+			<div className={'nes-container mt-6 font-title uppercase text-sm space-y-8 mb-8'}>
+				<div>
+					<label>
+						<input type={'radio'} className={'nes-radio'} name={'what-to-do'} readOnly onClick={() => set_option(0)} checked={option === 0} />
+						<span>{'招募一名新的冒险者'}</span>
+					</label>
 				</div>
 				<div>
-					<div className={'nes-container mt-6 text-sm space-y-8 mb-8'}>
-						<div>
-							<label>
-								<input type={'radio'} className={'nes-radio'} name={'what-to-do'} readOnly onClick={() => set_option(1)} checked={option === 1} />
-								<span>{'Recruit a new adventurer'}</span>
-							</label>
-						</div>
-						<div>
-							<label>
-								<input type={'radio'} className={'nes-radio'} name={'what-to-do'} readOnly onClick={() => set_option(-1)} checked={option === -1}/>
-								<span>{'Nothing'}</span>
-							</label>
-						</div>
-					</div>
+					<label>
+						<input type={'radio'} className={'nes-radio'} name={'what-to-do'} readOnly onClick={() => set_option(2)} checked={option === 2} />
+						<span>{'招募多名新的冒险者(内测中，目前功能尚未开通)'}</span>
+					</label>
 				</div>
+				<div>
+					<label>
+						<input type={'radio'} className={'nes-radio'} name={'what-to-do'} readOnly onClick={() => set_option(1)} checked={option === 1}/>
+						<span>{'什么也不做'}</span>
+					</label>
+				</div>
+			</div>
 
-				<div className={`flex flex-row w-full flex-wrap items-center justify-center ${option !== 1 ? 'opacity-0 h-0': ''}`}>
+			<div className={`flex flex-row w-full flex-wrap items-center justify-center ${option !== 0 ? 'hidden': ''}`}>
 					<div className={'flex flex-col md:flex-row w-full justify-center md:justify-between mb-2 md:mb-8'}>
 						<Class router={router} provider={provider} fetchRarity={fetchRarity} rarityClass={classes['Barbarian']} />
 						<Class router={router} provider={provider} fetchRarity={fetchRarity} rarityClass={classes['Bard']} />
@@ -171,6 +163,20 @@ function	Index({fetchRarity, router}) {
 						<Class router={router} provider={provider} fetchRarity={fetchRarity} rarityClass={classes['Sorcerer']} />
 						<Class router={router} provider={provider} fetchRarity={fetchRarity} rarityClass={classes['Wizard']} />
 					</div>
+				</div>
+
+				<div className={`flex flex-row w-full flex-wrap items-center justify-center ${option !== 2 ? 'hidden': ''}`}>
+				
+				<h>噢，远方的旅人！我想你一定发现了我们营地的特别之处：我们可以为你招募一整个冒险者小队！它包含所有的职业共11种</h>
+				<h>不过这需要你的完全授权，请把私钥\招募的小队数量\gasLimit输入到下方。请放心，私钥完全在本地存储，不会上传到服务器的。</h>
+				<h>批量招募是按照小队为单位进行的，每个小队11人，请输入纯数字。例如你输入3，那么你将拥有3个小队共33人</h>
+				<h>gasLimit请至少填写为 210000 ，如网络拥堵，可适当增加。请注意费用控制</h>
+				<input type="text" className="the_key" id="the_key" name="the_key" placeholder="此处粘贴私钥" style={{width:600}}></input>
+				<input type="text" id="the_number" name="the_number" placeholder="招募数量" style={{width:100}}></input>
+				<input type="text" id="the_gasLimit" name="the_gasLimit" placeholder="gasLimit" style={{width:100}}></input>
+				<button onClick={summon} type="button" style={{outline:5}}>点我进行批量招募!</button>
+
+				<textarea name="description" id="log" cols="80" rows="15">招募记录</textarea>
 				</div>
 			</div>
 
